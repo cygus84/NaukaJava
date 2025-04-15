aea2fb07716f70180d525b069c7adffb0414ff53
cygus84 <cygus84@gmail.com>
2025-04-15 15:12:48 +0100

    ac
    ac

--- Schronisko-003/src/app/StartApp.java
+++ Schronisko-003/src/app/StartApp.java
@@ -5,7 +5,7 @@ import okna.OknoGlowne;
 public class StartApp {
 
 	public static void main(String[] args) {
-		System.out.println("Start aplikacji schronisko-002!");
+		System.out.println("Start aplikacji schronisko-003!");
 		
 		//BD.dodajKota("Burek");
 		new OknoGlowne();
